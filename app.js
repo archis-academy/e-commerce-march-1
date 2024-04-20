13f997c592672ed99cf3da70264c4cc2f00ff2cc
Harun Karakaya <harunkrkya02@gmail.com>
2024-04-20 22:32:19 +0300

    [E--3]-indexcss-appjs-templated

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-  function changeColor(browse) {
-     let hompageBrowseBox = browse;
-     let hompageBrowseBoxActive = document.querySelector('.browse.active');
+function changeColor(browse) {
+    let hompageBrowseBox = browse;
+    let hompageBrowseBoxActive = document.querySelector('.browse.active');
 
-     if (hompageBrowseBoxActive) {
-         hompageBrowseBoxActive.classList.remove('active');
-       hompageBrowseBoxActive.style.backgroundColor = '#FFFFFF';
-       hompageBrowseBoxActive.style.color = '#000000';
-     }
-
-     hompageBrowseBox.classList.add('active');
-     hompageBrowseBox.style.backgroundColor = '#db4444';
-     hompageBrowseBox.style.color = '#FFFFFF';
-
-    
-
-     if (hompageBrowseBox.style.backgroundColor === '#db4444') {
-       hompageBrowseBox.style.backgroundColor = '#FFFFFF';
-      hompageBrowseBox.style.color = '#000000';
-     } else {
-       hompageBrowseBox.style.backgroundColor = '#db4444';
-       hompageBrowseBox.style.color = '#FFFFFF';
-     }
+    if (hompageBrowseBoxActive && hompageBrowseBoxActive !== hompageBrowseBox) {
+        hompageBrowseBoxActive.classList.remove('active');
+        hompageBrowseBoxActive.style.backgroundColor = '#FFFFFF';
+        hompageBrowseBoxActive.style.color = '#000000';
     }
+
+    if (!hompageBrowseBox.classList.contains('active')) {
+        hompageBrowseBox.classList.add('active');
+        hompageBrowseBox.style.backgroundColor = '#db4444';
+        hompageBrowseBox.style.color = '#FFFFFF';
+    } else {
+        hompageBrowseBox.classList.remove('active');
+        hompageBrowseBox.style.backgroundColor = '#FFFFFF';
+        hompageBrowseBox.style.color = '#000000';
+    }
+}
 
