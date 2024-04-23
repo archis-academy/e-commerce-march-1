18da8254682891c8722bd87e58fa0c6079697dbe
Harun Karakaya <harunkrkya02@gmail.com>
2024-04-23 16:05:56 +0300

    [E--5]-indexhtml-indexcss-appjs-templated

--- app.js
+++ app.js
@@ -19,3 +19,54 @@ function changeColor(browse) {
     }
 }
 
+
+
+
+const products = [
+    { name: "Product 1", price: 29.99 },
+    { name: "Product 2", price: 39.99 },
+    { name: "Product 3", price: 49.99 },
+  ];
+  function selectRandomProduct() {
+    const randomIndex = Math.floor(Math.random() * products.length);
+    return products[randomIndex];
+  }
+  const randomProduct = selectRandomProduct();
+
+  const smallTitle = document.querySelector(".hompage-featured-small-title h4");
+  const bigTitle = document.querySelector(".hompage-featured-big-title h1");
+  const timeBoxes = document.querySelectorAll(".hompage-time-box1 h1");
+  const buyNowButton = document.querySelector(".hompage-featured-button button");
+  
+  smallTitle.textContent = "Featured Product";
+  bigTitle.innerHTML = `Enhance Your <br />${randomProduct.name} Experience`;
+  
+  const countdownDate = new Date().getTime() + 24 * 60 * 60 * 1000;
+  
+  const countdown = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+  
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    timeBoxes[0].textContent = days;
+    timeBoxes[1].textContent = hours;
+    timeBoxes[2].textContent = minutes;
+    timeBoxes[3].textContent = seconds;
+  
+    if (distance < 0) {
+      clearInterval(countdown);
+      
+    }
+  }, 1000);
+  buyNowButton.addEventListener("click", () => {
+    
+    console.log(`"${randomProduct.name}" added to cart`);
+  
+    
+
+  });
+  
