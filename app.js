c18fb081c70a413b91a82e0eeab6514cc1119836
Deniz Kandemir <denizkandemir137@gmail.com>
2024-04-23 20:02:08 +0300

    "wishlist codes for bestselling added"

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ function DisplayProducts(any_Products) {
                <img class="best-selling-products-img" src="${product.image}"/>
                 <div class="best-selling-products-icons-container">
                  <div class="selling-wishlist-icon">             
-                  <button id="selling-products-wishlist-button" class="selling-products-button"><img id="selling-wishlist-icon-img" class="best-selling-products-icons" src="images/wishlist-icon.svg"/></button>
+                  <button onclick="addToWishlist(${product.id})" id="selling-products-wishlist-button" class="selling-products-button">
+                    <svg width="32" class="selling-products-wishlist-svg" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                  </button>
                  </div>
-                 <div class="best-selling-eye-icon">
-                  <button id="selling-products-eye-button" class="selling-products-button"><img class="best-selling-products-icons" src="images/icon-eye.svg"/></button>
-                 </div> 
                 </div>  
               </div>
               <div class="best-selling-products-features">
@@ -28,22 +29,22 @@ function DisplayProducts(any_Products) {
             </div>`
   }).join("");
 
-  wishlistButtons = sellingProductsContainer.querySelectorAll("#selling-products-wishlist-button");
+  wishlistButtons = sellingProductsContainer.querySelectorAll(".selling-products-wishlist-svg");
 
 
   wishlistButtons.forEach(button => {
-    let buttonClicked = false;
+    let buttonClicked = true;
+
     button.addEventListener("click", () => {
-      if (!buttonClicked) {
-        button.style.background = "#DB4444";
-        buttonClicked = true;
+      if (buttonClicked) {
+        button.style.fill = "crimson";
+        buttonClicked = false;
       } else {
-        button.style.background = "none";
-       buttonClicked = false;
+        button.style.fill = "none";
+       buttonClicked = true;
       }
     });
-  });
-
+  }); 
 };
 
 async function GetProducts() {
@@ -59,6 +60,23 @@ function discount(any_products) {
 
 GetProducts();
 
+function addToWishlist(productId) {
+  console.log(productId);
+  console.log(bestSellingProducts);
+  const wishListProducts =  JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+
+  const addedProduct = bestSellingProducts.find((product) => product.id === productId);
+  
+  const checkProduct = wishListProducts.some((product) => product.id === productId);
+
+  if(!checkProduct) {
+    localStorage.setItem(
+      "wishlistProducts", JSON.stringify([...wishListProducts,addedProduct])
+    );
+  } else {
+    alert("This product is already in your wishlist");
+  }
+};
 
 
 
