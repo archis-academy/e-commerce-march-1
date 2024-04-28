380a5af46a91ee912554fd113830a2f5560f8627
Deniz Kandemir <denizkandemir137@gmail.com>
2024-04-28 22:16:42 +0300

    "Best selling codes about local storage updated "

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ async function DisplayProducts() {
                <img class="best-selling-products-img" src="${product.image}"/>
                 <div class="best-selling-products-icons-container">
                  <div onclick="addToWishlist(${product.id})"  class="selling-wishlist-icon">                
-                  <svg width="32" id="${product.id}" class="selling-products-wishlist-svg" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <svg width="32" id="wishlist_${product.id}" class="selling-products-wishlist-svg" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                    <path d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>  
                 </div> 
@@ -37,27 +37,35 @@ async function DisplayProducts() {
                 </div>  
                 <div class="bestselling-rating-container">
                   <img src="images/stars.png" class="bestselling-stars-img"/>
-                  <div class="bestselling-transparent-div" id="transparent-div" style="width:${hideStars(product)}%"></div>
-                </div>  
+                  <div class="bestselling-transparent-div" id="transparent-div" style="width:${hideStars(product)}%"></div>   
+                  <p class="best-selling-rating">${product.rating.rate}</p>       
+                </div>             
               </div>
             </div>`
   }).join("");
 
   const wishlistButtons = sellingProductsContainer.querySelectorAll(".selling-products-wishlist-svg");
-
-  wishlistButtons.forEach(button => {
+  wishlistButtons.forEach((button) => {
     let buttonClicked = true;
 
-    button.addEventListener("click", () => {
-      if (buttonClicked) {
+    button.addEventListener(("click"), () => {
+      if(buttonClicked  && button.style.fill !== "crimson") {
         button.style.fill = "crimson";
         buttonClicked = false;
-      } else {
-        button.style.fill = "none";
-        buttonClicked = true;
+      }
+      else {
+         button.style.fill = "none";
+         buttonClicked = true;
       }
     });
-  });
+ });   
+
+  const colorizedProducts =  JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+  colorizedProducts.map((product) => {
+    const redHearts = sellingProductsContainer.querySelector(`#wishlist_${product.id}`);
+    console.log(redHearts);
+    redHearts.style.fill = "crimson";
+   });
 };
 
 function discount(any_products) {
@@ -70,31 +78,36 @@ function ratingMaker(any_rate) {
 
 function hideStars(product) {
     const starRatio = ratingMaker(product.rating.rate);
-    console.log(starRatio);
     return starRatio
 };
 
 DisplayProducts();
 
 function addToWishlist(productId) {
-  console.log(productId);
-  console.log(bestSellingProducts);
-
   wishListProducts = JSON.parse(localStorage.getItem("wishlistProducts")) || [];
 
-  const addedProduct = bestSellingProducts.find((product) => product.id === productId);
-
+  const clickedProduct = bestSellingProducts.find((product) => product.id === productId);
   const checkProduct =  wishListProducts.some((product) => product.id === productId);
-
+  
   if (!checkProduct) {
     localStorage.setItem(
-      "wishlistProducts", JSON.stringify([...wishListProducts, addedProduct])
+      "wishlistProducts", JSON.stringify([...wishListProducts, clickedProduct])
     );
   } else {
-    alert("This product is already in your wishlist");
-  }
+      removeFromWishlist(productId);
+  };
 };
 
-console.log(addToWishlist());
+function removeFromWishlist(productId) {
+   const indexToRemove = wishListProducts.findIndex((product) => product.id === productId)
+   
+   wishListProducts.splice(indexToRemove,1);
+
+   localStorage.setItem (
+    "wishlistProducts" , JSON.stringify([...wishListProducts])
+   );
+};
 
 //Best Selling Ends
+
+//Wishlist starts
