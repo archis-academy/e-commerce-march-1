d26ba96782898253852078fccb46fdf76546faf4
Deniz Kandemir <denizkandemir137@gmail.com>
2024-04-29 00:16:25 +0300

    "AddToCard local storage js codes updated"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const sellingProductsContainer = document.querySelector("#best-selling-api-products");
 
 let wishListProducts = [];
+let cardProducts = [];
 let bestSellingProducts;
 
 async function DisplayProducts() {
@@ -19,8 +20,8 @@ async function DisplayProducts() {
                    <path d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>  
                 </div> 
-                 <div class="selling-card-icon-container">
-                   <svg width="32" height="32"  class="selling-card-icon" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <div onclick="addToCard(${product.id})" class="selling-card-icon-container">
+                   <svg width="32" height="32" id="card_${product.id}" class="selling-card-icon" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                      <path d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                      <path d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                      <path d="M3 5H7L10 22H26" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -60,12 +61,36 @@ async function DisplayProducts() {
     });
  });   
 
-  const colorizedProducts =  JSON.parse(localStorage.getItem("wishlistProducts")) || [];
-  colorizedProducts.map((product) => {
-    const redHearts = sellingProductsContainer.querySelector(`#wishlist_${product.id}`);
-    console.log(redHearts);
-    redHearts.style.fill = "crimson";
+  const colorizedWishlistProducts =  JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+  colorizedWishlistProducts.map((product) => {
+    const wishlistSvgs = sellingProductsContainer.querySelector(`#wishlist_${product.id}`);
+    console.log(wishlistSvgs);
+    wishlistSvgs.style.fill = "crimson";
    });
+
+   const cardButtons = sellingProductsContainer.querySelectorAll(".selling-card-icon");
+   
+   cardButtons.forEach((button) => {
+    let buttonClicked = true;
+    
+    button.addEventListener(("click"), () => {
+      if(buttonClicked && button.style.fill !== "dimgray") {
+        button.style.fill = "dimgray";
+        buttonClicked = false;
+      }
+      else {
+        button.style.fill = "none";
+        buttonClicked = true;
+      }
+    });
+  });
+
+  const colorizedCardProducts = JSON.parse(localStorage.getItem("cardProducts")) || [];
+  colorizedCardProducts.map((product) => {
+    const cardSvgs = sellingProductsContainer.querySelector(`#card_${product.id}`);
+    console.log(cardSvgs);
+    cardSvgs.style.fill = "dimgray";
+  });
 };
 
 function discount(any_products) {
@@ -105,6 +130,31 @@ function removeFromWishlist(productId) {
 
    localStorage.setItem (
     "wishlistProducts" , JSON.stringify([...wishListProducts])
+   );
+};
+
+function addToCard(productId) {
+  cardProducts = JSON.parse(localStorage.getItem("cardProducts")) || [];
+
+  const clickedForCard = bestSellingProducts.find((product) => product.id === productId);
+  const checkCardProduct = cardProducts.some((product) => product.id === productId);
+
+  if(!checkCardProduct) {
+    localStorage.setItem(
+      "cardProducts" , JSON.stringify([...cardProducts,clickedForCard])
+    );
+  }
+  else {
+    removeFromCard(productId);
+  }
+};
+
+function removeFromCard(productId) {
+   const checkCardIndex = cardProducts.findIndex((product) => product.id === productId);
+   
+   cardProducts.splice(checkCardIndex,1);
+   localStorage.setItem(
+     "cardProducts", JSON.stringify([...cardProducts])
    );
 };
 
