ba11b2fd92d5d0debeb859a5a61fa84871fdbf04
Deniz Kandemir <denizkandemir137@gmail.com>
2024-05-01 16:24:35 +0300

    [E--4]indexHtml-indexCs-appJs styles corrected"

--- app.js
+++ app.js
@@ -21,21 +21,21 @@ async function DisplayBestSellingProducts() {
                     <path d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                    </svg>  
                  </div> 
-                  <div onclick="addToCard(${product.id})" class="selling-card-icon-container">
-                    <svg width="32" height="32" id="card_${product.id}" class="selling-card-icon" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <div  onclick="addToCard(${product.id})"  id="card_${product.id}" class="selling-card-icon-container">
+                    <svg width="24px" height="24px" class="selling-card-icon" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M3 5H7L10 22H26" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                       <path d="M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97371 27.8239 6.90678 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66661 27.39 6.66666H8" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                   </div>
-                 </div>  
+                    </svg>        
+                  </div>   
+                </div>  
               </div>
                <div class="best-selling-products-features">
                  <p class="best-selling-titles">${product.title}</p>
                  <div class="best-selling-discount">
-                   <p class="discount-price">${discount(product).toFixed(2)}</p> 
-                   <s>${product.price}</s>
+                   <p class="discount-price">${discount(product).toFixed(2)}$</p> 
+                   <s>${product.price}$</s>
                  </div>  
                  <div class="bestselling-rating-container">
                    <img src="images/stars.png" class="bestselling-stars-img"/>
@@ -46,6 +46,12 @@ async function DisplayBestSellingProducts() {
               </div> 
             </div>`
   }).join("");
+
+  const colorizedWishlistProducts =  JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+  colorizedWishlistProducts.map((product) => {
+    const wishlistSvgs = sellingProductsContainer.querySelector(`#wishlist_${product.id}`);
+    wishlistSvgs.style.fill = "crimson";
+  });
 
   const wishlistButtons = sellingProductsContainer.querySelectorAll(".selling-products-wishlist-svg");
   wishlistButtons.forEach((button) => {
@@ -63,35 +69,33 @@ async function DisplayBestSellingProducts() {
     });
  });   
 
-  const colorizedWishlistProducts =  JSON.parse(localStorage.getItem("wishlistProducts")) || [];
-  colorizedWishlistProducts.map((product) => {
-    const wishlistSvgs = sellingProductsContainer.querySelector(`#wishlist_${product.id}`);
-    console.log(wishlistSvgs);
-    wishlistSvgs.style.fill = "crimson";
-   });
+ const colorizedCardProducts = JSON.parse(localStorage.getItem("cardProducts")) || [];
+ colorizedCardProducts.map((product) => {
+   const cardDivs = sellingProductsContainer.querySelector(`#card_${product.id}`);
+   cardDivs.innerHTML = `<i class="selling-card-tick-icon fa-sharp fa-solid fa-check"></i>`;     
+  });
+  
 
-   const cardButtons = sellingProductsContainer.querySelectorAll(".selling-card-icon");
-   
-   cardButtons.forEach((button) => {
+  const cardContainer = sellingProductsContainer.querySelectorAll(".selling-card-icon-container");
+
+  cardContainer.forEach((container) => {
     let buttonClicked = true;
-    
-    button.addEventListener(("click"), () => {
-      if(buttonClicked && button.style.fill !== "dimgray") {
-        button.style.fill = "dimgray";
+
+    container.addEventListener(("click") , () => {
+      if(buttonClicked && container.innerHTML !== `<i class="selling-card-tick-icon fa-sharp fa-solid fa-check"></i>`) {
+        container.innerHTML = `<i class="selling-card-tick-icon fa-sharp fa-solid fa-check"></i>`
         buttonClicked = false;
       }
-      else {
-        button.style.fill = "none";
+      else {   
+        container.innerHTML =  `<svg width="24px" height="24px" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                  <path d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                  <path d="M3 5H7L10 22H26" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                  <path d="M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97371 27.8239 6.90678 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66661 27.39 6.66666H8" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                               </svg>`
         buttonClicked = true;
       }
     });
-  });
-
-  const colorizedCardProducts = JSON.parse(localStorage.getItem("cardProducts")) || [];
-  colorizedCardProducts.map((product) => {
-    const cardSvgs = sellingProductsContainer.querySelector(`#card_${product.id}`);
-    console.log(cardSvgs);
-    cardSvgs.style.fill = "dimgray";
   });
 };
 
