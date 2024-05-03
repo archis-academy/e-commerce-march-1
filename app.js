3cfe5a1edef56b0a96816d3c479a7351089df4e7
tunahan arslan <tunahanarslan55@hotmail.com>
2024-05-03 19:12:46 +0300

    [E--6]-cart-products-added-in-local-storage

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let products = [];
 let product = [];
 let scrollAmount = 0;
 let wishlistProducts = [];
+let cartProducts = [];
 
 async function GetProducts() {
   const response = await fetch("https://fakestoreapi.com/products");
@@ -16,17 +17,21 @@ async function GetProducts() {
                 <img class="explore-product-img" src="${product.image}" alt="" />
             </div>
             <div class="explore-wishlist-and-cart-icon">
-                <div onclick="AddLocalStorage(${product.id})" class="wishlist-icon-box">
+                <div onclick="AddToWishlist(${product.id})" class="wishlist-icon-box">
                     <svg id="wishlist-icon-${product.id}" class="explore-wishlist-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path id="wishlist-stroke-${product.id}" d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </div>
-                <div class="explore-cart-icon-box">
-                    <svg class=explore-cart-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <div onclick="AddToCart(${product.id})" id="explore-cart-icon-box-${product.id}" class="explore-cart-icon-box">
+                    <svg id="cart-icon-${product.id}" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M3 5H7L10 22H26" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M10 16.6667H25.59C25.7056 16.6667 25.8177 16.6267 25.9072 16.5535C25.9966 16.4802 26.0579 16.3782 26.0806 16.2648L27.8806 7.26479C27.8951 7.19222 27.8934 7.11733 27.8755 7.04552C27.8575 6.97371 27.8239 6.90678 27.7769 6.84956C27.73 6.79234 27.6709 6.74625 27.604 6.71462C27.5371 6.68299 27.464 6.66661 27.39 6.66666H8" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+
+                    <svg id="check-icon-${product.id}" width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     </svg>
                 </div>
             </div> 
@@ -86,19 +91,36 @@ async function GetProducts() {
         }
       }
     });
+
+    const storedCartProducts =
+      JSON.parse(localStorage.getItem("cartProduct")) || [];
+
+    products.forEach((product) => {
+      const isFavorite = storedCartProducts.some(
+        (cartProduct) => cartProduct.id === product.id
+      );
+
+      if (isFavorite) {
+        cartIcon = document.getElementById(`cart-icon-${product.id}`);
+        if (cartIcon) {
+          cartIcon.style.display = "none";
+        }
+      } else {
+        checkIcon = document.getElementById(`check-icon-${product.id}`);
+        if (checkIcon) {
+          checkIcon.style.display = "none";
+        }
+      }
+    });
   });
 }
 GetProducts();
 
-function AddLocalStorage(productId) {
+function AddToWishlist(productId) {
+  wishlistProducts = JSON.parse(localStorage.getItem("wishlistProduct")) || [];
   const newWishlistProduct = products.find(
     (product) => product.id === productId
   );
-  const testLocale = localStorage.getItem("wishlistProduct");
-  if (testLocale) {
-    wishlistProducts = JSON.parse(testLocale);
-  }
-
   const isMatch = wishlistProducts.find(
     (product) => product.id === newWishlistProduct.id
   );
@@ -113,11 +135,11 @@ function AddLocalStorage(productId) {
     );
     WishlistIconStroke.setAttribute("stroke", "#db4444");
   } else {
-    DeleteLocalStorage(productId);
+    DeleteFromWishlist(productId);
   }
 }
 
-function DeleteLocalStorage(productId) {
+function DeleteFromWishlist(productId) {
   const newWishlistProducts = wishlistProducts.filter(
     (product) => product.id !== productId
   );
@@ -131,12 +153,43 @@ function DeleteLocalStorage(productId) {
   WishlistIconStroke.setAttribute("stroke", "black");
 }
 
+function AddToCart(productId) {
+  cartProducts = JSON.parse(localStorage.getItem("cartProduct")) || [];
+  const newCartProduct = products.find((product) => product.id === productId);
+  const isMatch = cartProducts.find(
+    (product) => product.id === newCartProduct.id
+  );
+  if (!isMatch) {
+    const productAdd = [...cartProducts, newCartProduct];
+    localStorage.setItem("cartProduct", JSON.stringify(productAdd));
+    cartProducts = productAdd;
+    cartIcon = document.getElementById(`cart-icon-${productId}`);
+    cartIcon.style.display = "none";
+    checkIcon = document.getElementById(`check-icon-${productId}`);
+    checkIcon.style.display = "";
+  } else {
+    DeleteFromCart(productId);
+  }
+}
+
+function DeleteFromCart(productId) {
+  const newCartProducts = cartProducts.filter(
+    (product) => product.id !== productId
+  );
+  localStorage.setItem("cartProduct", JSON.stringify(newCartProducts));
+  cartProducts = newCartProducts;
+  checkIcon = document.getElementById(`check-icon-${productId}`);
+  checkIcon.style.display = "none";
+  cartIcon = document.getElementById(`cart-icon-${productId}`);
+  cartIcon.style.display = "";
+}
+
 const productlist = document.querySelector(".explore-products-box");
 const rightArrow = document.querySelector(".explore-right-arrow");
 
 rightArrow.addEventListener("click", () => {
   if (scrollAmount < productlist.scrollWidth - productlist.clientWidth) {
-    scrollAmount += 400;
+    scrollAmount += 340;
     productlist.style.transform = `translateX(0px)`;
     productlist.style.transform = `translateX(${-scrollAmount}px)`;
   }
@@ -145,7 +198,7 @@ rightArrow.addEventListener("click", () => {
 const leftArrow = document.querySelector(".explore-left-arrow");
 leftArrow.addEventListener("click", () => {
   if (scrollAmount > 0) {
-    scrollAmount -= 400;
+    scrollAmount -= 340;
     productlist.style.transform = `translateX(0px)`;
     productlist.style.transform = `translateX(${-scrollAmount}px)`;
   }
