f7e065df3143b91760115f059d9d4df82eb524ff
Deniz Kandemir <denizkandemir137@gmail.com>
2024-05-07 18:43:10 +0300

    [E--11]cartHTML-appJS-coupon and quantity codes added

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-function changeColor(browse) {
-    let hompageBrowseBox = browse;
-    let hompageBrowseBoxActive = document.querySelector('.browse.active');
-
-    if (hompageBrowseBoxActive && hompageBrowseBoxActive !== hompageBrowseBox) {
-        hompageBrowseBoxActive.classList.remove('active');
-        hompageBrowseBoxActive.style.backgroundColor = '#FFFFFF';
-        hompageBrowseBoxActive.style.color = '#000000';
-    }
-
-    if (!hompageBrowseBox.classList.contains('active')) {
-        hompageBrowseBox.classList.add('active');
-        hompageBrowseBox.style.backgroundColor = '#db4444';
-        hompageBrowseBox.style.color = '#FFFFFF';
-    } else {
-        hompageBrowseBox.classList.remove('active');
-        hompageBrowseBox.style.backgroundColor = '#FFFFFF';
-        hompageBrowseBox.style.color = '#000000';
-    }
-}
-
-
-
 //Cart starts
 
 const cartPageProducts = JSON.parse(localStorage.getItem("cartProducts")) || [];
@@ -30,7 +7,7 @@ function displayCartProducts() {
   if(cartPageProducts.length === 0) {
     cartProductsContainer.innerHTML = `<div class="cart-products-features-container" style="padding: 30px">
                                          <div class="cart-empty-text-svg-container">
-                                          <p class="cart-empty-text"> You do not have any products in your cart </p>
+                                          <p class="cart-empty-text"> You do not have any products in your Cart </p>
                                           <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M11 27C11.5523 27 12 26.5523 12 26C12 25.4477 11.5523 25 11 25C10.4477 25 10 25.4477 10 26C10 26.5523 10.4477 27 11 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                                             <path d="M25 27C25.5523 27 26 26.5523 26 26C26 25.4477 25.5523 25 25 25C24.4477 25 24 25.4477 24 26C24 26.5523 24.4477 27 25 27Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -47,24 +24,27 @@ function displayCartProducts() {
                 <img src="${product.image}" class="cart-products-images"/>
                 <p class="cart-products-names"> ${product.title} </p>
                </div>      
-               <p class="cart-products-price"> ${product.price} </p>
-               <input type="number" min="0" value="01" id="product-quantity-${product.id}" class="cart-products-quantity"/> 
-               <p id="price-${product.id}"> ${product.price} </p>
+               <p class="cart-products-price"> ${product.price}$ </p>
+               <input type="number" min="0" value="1" id="product-quantity-${product.id}" class="cart-products-quantity"/> 
+               <p id="price-${product.id}"> ${product.price.toFixed(1)}$ </p>
               </div>`
     }).join("");  
   } 
-  
 };
 
 displayCartProducts();
 
-function totalCalculations() {
-  cartPageProducts.map((product) => {
-    const quentities = cartProductsContainer.querySelector(`#product-quantity-${product.id}`);  
 
-    quentities.addEventListener(("keyup") , () => {    
-      localStorage.setItem(`quentity-${product.id}` , JSON.stringify(quentities.value));
+function subtotalCalculations() {
+  cartPageProducts.map((product) => {
+    const quantity = cartProductsContainer.querySelector(`#product-quantity-${product.id}`); 
+    quantity.addEventListener(("keyup") , () => {    
+      localStorage.setItem(`quantity-${product.id}` , JSON.stringify(quantity.value));
       updateSubtotals();
+  });
+  quantity.addEventListener(("click") , () => {    
+    localStorage.setItem(`quantity-${product.id}` , JSON.stringify(quantity.value));
+    updateSubtotals();
   });
  }); 
 }
@@ -72,18 +52,123 @@ function totalCalculations() {
 function updateSubtotals() {
   const updateButton = document.querySelector("#cart-update-button");
   cartPageProducts.map((product) => {
-    const subtotals = cartProductsContainer.querySelector(`#price-${product.id}`);
-    const  storedQuentities = JSON.parse(localStorage.getItem(`quentity-${product.id}`)) || [];
+    const subtotal = cartProductsContainer.querySelector(`#price-${product.id}`);
+    const storedQuantities = JSON.parse(localStorage.getItem(`quantity-${product.id}`)) || [];
+    const intQuantities = parseInt(storedQuantities);
+    let storedSubtotals;
+
     updateButton.addEventListener(("click") , () => {
-      if(storedQuentities > 0) {
-        subtotals.textContent = product.price*storedQuentities;
+      if(intQuantities > 0) {
+        subtotal.textContent = `${(product.price*storedQuantities).toFixed(2)}$`;
+        storedSubtotals = subtotal.textContent;
+        localStorage.setItem(`subtotal-${product.id}` , JSON.stringify(storedSubtotals));
+        window.location.reload();
+      }
+      else if(intQuantities === 0){
+        const deletedProduct = cartPageProducts.findIndex((productToDelete) => productToDelete.id === product.id);
+        cartPageProducts.splice(deletedProduct,1);
+        localStorage.setItem("cartProducts", JSON.stringify([...cartPageProducts]));
+        window.location.reload();
       }
     });
-    const updatedSubtotals = subtotals.textContent;
   });
 };
 
+function getStoredValues() {
+  cartPageProducts.map((product) => {
+    const subtotal = cartProductsContainer.querySelector(`#price-${product.id}`); 
+    const quantity = cartProductsContainer.querySelector(`#product-quantity-${product.id}`); 
+    const getSubtotal = JSON.parse(localStorage.getItem(`subtotal-${product.id}`));
+    const getQuantity = JSON.parse(localStorage.getItem(`quantity-${product.id}`));
+    const intSubtotal = parseInt(getSubtotal);
+    const intQuantity = parseInt(getQuantity);
 
-totalCalculations();
+    if(intSubtotal > 0 && intSubtotal !== undefined) {
+      subtotal.textContent = getSubtotal;
+    }
+    if(intQuantity > 0 && intQuantity !== undefined) { 
+      quantity.value = getQuantity;
+    }  
+  });
+};
+
+const checkoutTotal = document.querySelector("#cart-calculate-total");
+
+function checkoutTotalCalculations() {
+  const checkoutSubtotal = document.querySelector("#cart-calculate-subtotal");
+  const cardShippingContainer = document.querySelector("#cart-shipping-subtotal");
+  const cardShippingValue = calculateShipping();
+  cardShippingContainer.textContent = `${cardShippingValue}$`;
+
+  let cardSum = 0;
+  cartPageProducts.forEach((product) => {
+    const quantity = cartProductsContainer.querySelector(`#product-quantity-${product.id}`); 
+    const intQuantity = parseInt(quantity.value);
+    cardSum  += (intQuantity * product.price);  
+  });
+
+  checkoutSubtotal.textContent = `${cardSum.toFixed(2)}$`;
+  checkoutTotal.textContent = `${(cardSum + cardShippingValue).toFixed(2)}$`
+};
+
+function calculateShipping() {
+  let totalQuantity = 0;
+  cartPageProducts.forEach((product) => {
+    const quantity = cartProductsContainer.querySelector(`#product-quantity-${product.id}`); 
+    const intQuantity = parseInt(quantity.value);
+    totalQuantity += (intQuantity);
+  });
+
+  if(totalQuantity === 0) 
+   {return 10;}
+  else if(totalQuantity <= 5) 
+   {return 15;}
+  else if(totalQuantity <= 10) 
+   {return 20;}
+  else {return 30;}
+};
+
+function applyCoupun(total) {
+  const applyButton = document.querySelector("#cart-coupon-apply-button");
+  const couponInput = document.querySelector("#cart-coupon-input");
+  let couponDiscount = 1;
+
+  applyButton.addEventListener(("click") , () => {
+     const couponText = couponInput.value;
+     couponDiscount = checkCoupon(couponText);
+     const totalBeforeCoupon = parseFloat(total.textContent);
+     total.textContent = `${(totalBeforeCoupon - (totalBeforeCoupon*couponDiscount)).toFixed(2)}$`;
+     if(couponDiscount === 0) {
+      alert("The coupon is unvalid");
+     }
+     else { alert("Coupon applied to your total")}
+  });
+};
+
+function checkCoupon(couponText) {
+ switch (couponText) {
+  case "discount20":
+    return 20/100;
+    break;
+  case "discount50":
+    return 50/100;
+    break;
+  case "discount75":
+    return 75/100;      
+    break;
+  case "gift100":
+    return 1;
+  default:
+    return 0;
+    break;
+ }
+};
+     
+subtotalCalculations();
+getStoredValues();
+checkoutTotalCalculations();            
+applyCoupun(checkoutTotal);
+
+
 
 //Cart Ends
