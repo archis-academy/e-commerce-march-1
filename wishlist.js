688c5681df702e27ceb94b18e81c8acd82fddb19
Alperen Üstün <alperen.ustun39@gmail.com>
2024-05-09 19:56:01 +0300

    [E-10] chore: Refactor wishlist page HTML and JavaScript

--- wishlist.js
+++ wishlist.js
@@ -1,70 +1,25 @@
-const product = [
-  {
-    id: 0,
-    image: "images/wishlist-bag.png",
-    title: "Gucci duffle bag",
-    price: 960,
-  },
-  {
-    id: 1,
-    image: "images/Frame-610.png",
-    title: "RGB liquid CPU Cooler",
-    price: 1960,
-  },
-  {
-    id: 2,
-    image: "images/Frame-608.png",
-    title: "GP11 Shooter USB Gamepad",
-    price: 550,
-  },
-  {
-    id: 3,
-    image: "images/jacket.png",
-    title: "Quilted Satin Jacket",
-    price: 750,
-  },
-];
-localStorage.setItem("products",JSON.stringify(product));
+const wishlistContainer = document.querySelector("#wishlistContainer");
+const wishlistCount = document.querySelector("#wishlistCount");
 
+function renderWishlistProducts() {
+  const wishlistProducts =
+    JSON.parse(localStorage.getItem("wishlistProducts")) || [];
 
-let savedProducts = JSON.parse(localStorage.getItem("products"));
+  wishlistCount.innerHTML = `Wishlist (${wishlistProducts.length})`;
 
-
-function removeFromWishlist(element) {
-  
-  let index = parseInt(element.parentNode.parentNode.getAttribute("data-id"));
-
-  
-  savedProducts = savedProducts.filter((item) => item.id !== index);
-
-  
-  localStorage.setItem("products", JSON.stringify(savedProducts));
-
-  
-  let wishlistCount = document.getElementById("wishlist(4)");
-
-  wishlistCount.textContent = `Wishlist(${savedProducts.length})`;
-
-  
-  renderWishlist(savedProducts);
-}
-
-
-function renderWishlist(products) {
-  let wishlistContainer = document.getElementById("wishlist-container");
-  wishlistContainer.innerHTML = products
+  wishlistContainer.innerHTML = wishlistProducts
     .map((item) => {
       const { id, image, title, price } = item;
       return `
-            <div class="wishlist-products-page" data-id="${id}">
+            <div class="wishlist-products-page" id="${id}">
                 <div class="wishlist-bag">
-                    <span onclick="removeFromWishlist(this)"><i class="far fa-trash-alt"></i></span>
+                    <i class="far fa-trash-alt" onclick="deleteFromWishlist(${id})"></i>
                     <img class="wishlist-images" src="${image}">
                     <button class="ad-to-cart"><i class="far fa-shopping-cart"></i>Add To Cart</button>
                 </div>
                 <div class="bag-text">
                     <h4>${title}</h4>
-                    <p>${price}</p>
+                    <p>₺${price}</p>
                 </div>
             </div>
         `;
@@ -72,7 +27,20 @@ function renderWishlist(products) {
     .join("");
 }
 
+function deleteFromWishlist(productId) {
+  const wishlistProducts =
+    JSON.parse(localStorage.getItem("wishlistProducts")) || [];
 
-document.addEventListener("DOMContentLoaded", function () {
-    renderWishlist(savedProducts);
-});
+  const updatedWishlistProducts = wishlistProducts.filter(
+    (product) => product.id !== productId
+  );
+
+  localStorage.setItem(
+    "wishlistProducts",
+    JSON.stringify(updatedWishlistProducts)
+  );
+
+  renderWishlistProducts();
+}
+
+renderWishlistProducts();
