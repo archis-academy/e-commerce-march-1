89f0fc4916502fbb378daba21dd022076d5ab4c2
Deniz Kandemir <denizkandemir137@gmail.com>
2024-06-11 02:58:55 +0300

    [E--15]appJs-indexCSS-searchBar-styles-and-functions-updated

--- app.js
+++ app.js
@@ -43,55 +43,31 @@ let healthCategory = {
   products : []
 };
 let allCategories = [womanCategory,manCategory,electronicsCategory,jeweleryCategory,medicineCategory,sportsCategory,toysCategory,petCategory,healthCategory];
-
-
+const advertiseContainer = document.querySelector("#slides-imgs-container");
 
 function checkAdvertiseImages() {
-  const advertiseContainer = document.querySelector("#slides-imgs-container");
   let windowWidth = window.innerWidth;
-
-  if (parseInt(windowWidth) > 549) {
-    advertiseContainer.innerHTML = 
-  ` <div class="header-advertise-img-container header-first-advertise" id="header-first-advertise">
-     <img class="header-advertise-img" src="images/advertise1.png" alt="">
-   </div>
-   <div class="header-advertise-img-container">
-     <img class="header-advertise-img" src="images/advertise2.png" alt="">
-   </div>
-   <div class="header-advertise-img-container">
-      <img class="header-advertise-img" src="images/advertise3.png" alt="">
-   </div>
-   <div class="header-advertise-img-container">
-      <img class="header-advertise-img" src="images/advertise4.png" alt="">
-   </div>
-   <div class="header-advertise-img-container">
-      <img class="header-advertise-img" src="images/advertise5.png" alt="">
-    </div>`
-  } else {
-    advertiseContainer.innerHTML = 
+  for (let i = 1; i < 6; i++) {
+    if (parseInt(windowWidth) > 549) {
+      advertiseContainer.innerHTML += 
     ` <div class="header-advertise-img-container header-first-advertise" id="header-first-advertise">
-       <img class="header-advertise-img" src="images/responsiveAdvertise1.png" alt="">
+       <img class="header-advertise-img" src="images/advertise${i}.png" alt="">
      </div>
-     <div class="header-advertise-img-container">
-       <img class="header-advertise-img" src="images/responsiveAdvertise2.png" alt="">
-     </div>
-     <div class="header-advertise-img-container">
-        <img class="header-advertise-img" src="images/responsiveAdvertise3.png" alt="">
-     </div>
-     <div class="header-advertise-img-container">
-        <img class="header-advertise-img" src="images/responsiveAdvertise4.png" alt="">
-     </div>
-     <div class="header-advertise-img-container">
-        <img class="header-advertise-img" src="images/responsiveAdvertise5.png" alt="">
-      </div>`
+     `
+    } else {
+      advertiseContainer.innerHTML += 
+      ` <div class="header-advertise-img-container header-first-advertise" id="header-first-advertise">
+         <img class="header-advertise-img" src="images/responsiveAdvertise${i}.png" alt="">
+       </div>`
+    }  
   }
-  
-
+ 
   firstAdvertise =  advertiseContainer.querySelector("#header-first-advertise");
 }
 
 checkAdvertiseImages();
 window.addEventListener(("resize") , () => {
+  advertiseContainer.innerHTML = ``;
   checkAdvertiseImages();
 })
 
@@ -119,7 +95,6 @@ function setTimeToSlides(advertiseCounter,transparentCounter,clickRadios,time) {
     showRadio.style.backgroundColor = "black";
    }
  
-  
    if (advertiseCounter > 1) {
      const currentMarginLeft = parseFloat(firstAdvertise.style.marginLeft) || 0;
      firstAdvertise.style.marginLeft = `${currentMarginLeft - 20}%`;
@@ -243,9 +218,13 @@ function determineProductCategories(allProducts) {
 let filteredProducts = [];
 const searchDropdown = document.querySelector("#search-dropdown"); 
 let searchApiProducts = searchDropdown.querySelector(".search-api-products");
+const searchSvg = document.querySelector(".search-svg");
+const deleteSvg = document.querySelector(".search-delete-svg");
+let searchValue = "";
 
 function displaySearchedProducts(allProducts) {
   const searchBar = document.querySelector(".header-search");
+  searchApiProducts.innerHTML = ``;
   allProducts.map((product) => {
     searchApiProducts.innerHTML += `<div class="search-products" id="search-products">
            <div class="category-products-features-container" id="search-products">          
@@ -257,15 +236,33 @@ function displaySearchedProducts(allProducts) {
  
   searchBar.addEventListener(("keyup") , () => {
    searchDropdown.style.display = "flex";
+   searchValue = searchBar.value
+   hideSearchSvg(searchValue);
    filteredProducts = [];
    filteredProducts = allProducts.filter((anyProduct) => anyProduct.title.toLowerCase().includes(searchBar.value.toLowerCase().trim()));
    displayFilteredProducts(filteredProducts,searchApiProducts);
    applyPopularSearch(allProducts)
   });
+
+  deleteSvg.addEventListener(("click") , () => {
+    searchBar.value = "";
+    searchSvg.style.display = "block";
+    deleteSvg.style.display = "none";
+    displaySearchedProducts(allProducts);
+  });
 };
 
+function hideSearchSvg(value) {
+  if (value !== "") {
+     searchSvg.style.display = "none";
+     deleteSvg.style.display = "block";
+   } else {
+    searchSvg.style.display = "block";
+    deleteSvg.style.display = "none";
+   }
+}
+
 function displayFilteredProducts(products,searchApiProducts) {
-  console.log("jkkjha")
   searchApiProducts.innerHTML = ``;
   products.map((product) => {
     searchApiProducts.innerHTML += 
@@ -287,22 +284,10 @@ function applyPopularSearch(allProducts) {
    popularSearches.forEach((search) => {
     search.addEventListener(("click"),() => {
      searchBar.value = search.textContent;
+     searchValue = searchBar.value;
+     hideSearchSvg(searchValue);
+     
      filteredProducts = [];
-     searchDropdown.innerHTML = 
-     `   <div class="popular-searches-title-container" id="search-products">
-             <p id="search-products" class="popular-searches-title">Popular Searches</p>
-             <?xml version="1.0" id="search-products" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
-             <svg width="25px" height="25px" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="iconify iconify--noto" preserveAspectRatio="xMidYMid meet"><radialGradient id="IconifyId17ecdb2904d178eab8626" cx="68.884" cy="124.296" r="70.587" gradientTransform="matrix(-1 -.00434 -.00713 1.6408 131.986 -79.345)" gradientUnits="userSpaceOnUse"><stop offset=".314" stop-color="#ff9800"></stop><stop offset=".662" stop-color="#ff6d00"></stop><stop offset=".972" stop-color="#f44336"></stop></radialGradient><path d="M35.56 40.73c-.57 6.08-.97 16.84 2.62 21.42c0 0-1.69-11.82 13.46-26.65c6.1-5.97 7.51-14.09 5.38-20.18c-1.21-3.45-3.42-6.3-5.34-8.29c-1.12-1.17-.26-3.1 1.37-3.03c9.86.44 25.84 3.18 32.63 20.22c2.98 7.48 3.2 15.21 1.78 23.07c-.9 5.02-4.1 16.18 3.2 17.55c5.21.98 7.73-3.16 8.86-6.14c.47-1.24 2.1-1.55 2.98-.56c8.8 10.01 9.55 21.8 7.73 31.95c-3.52 19.62-23.39 33.9-43.13 33.9c-24.66 0-44.29-14.11-49.38-39.65c-2.05-10.31-1.01-30.71 14.89-45.11c1.18-1.08 3.11-.12 2.95 1.5z" fill="url(#IconifyId17ecdb2904d178eab8626)"></path><radialGradient id="IconifyId17ecdb2904d178eab8627" cx="64.921" cy="54.062" r="73.86" gradientTransform="matrix(-.0101 .9999 .7525 .0076 26.154 -11.267)" gradientUnits="userSpaceOnUse"><stop offset=".214" stop-color="#fff176"></stop><stop offset=".328" stop-color="#fff27d"></stop><stop offset=".487" stop-color="#fff48f"></stop><stop offset=".672" stop-color="#fff7ad"></stop><stop offset=".793" stop-color="#fff9c4"></stop><stop offset=".822" stop-color="#fff8bd" stop-opacity=".804"></stop><stop offset=".863" stop-color="#fff6ab" stop-opacity=".529"></stop><stop offset=".91" stop-color="#fff38d" stop-opacity=".209"></stop><stop offset=".941" stop-color="#fff176" stop-opacity="0"></stop></radialGradient><path d="M76.11 77.42c-9.09-11.7-5.02-25.05-2.79-30.37c.3-.7-.5-1.36-1.13-.93c-3.91 2.66-11.92 8.92-15.65 17.73c-5.05 11.91-4.69 17.74-1.7 24.86c1.8 4.29-.29 5.2-1.34 5.36c-1.02.16-1.96-.52-2.71-1.23a16.09 16.09 0 0 1-4.44-7.6c-.16-.62-.97-.79-1.34-.28c-2.8 3.87-4.25 10.08-4.32 14.47C40.47 113 51.68 124 65.24 124c17.09 0 29.54-18.9 19.72-34.7c-2.85-4.6-5.53-7.61-8.85-11.88z" fill="url(#IconifyId17ecdb2904d178eab8627)"></path></svg>
-        </div> 
-         <div id="search-products" class="popular-searches-container">     
-          <p class="header-poppins-p popular-searches-p" id="search-products">Jacket</p>
-          <p class="header-poppins-p popular-searches-p" id="search-products">Cotton</p>
-          <p class="header-poppins-p popular-searches-p" id="search-products">SSD</p>
-          <p class="header-poppins-p popular-searches-p" id="search-products">Monitor</p>
-          <p class="header-poppins-p popular-searches-p" id="search-products">Gold</p>
-          <p class="header-poppins-p popular-searches-p" id="search-products">Coat</p>
-         </div>
-         <div class="search-api-products"></div>`;
      filteredProducts = allProducts.filter((anyProduct) =>  anyProduct.title.toLowerCase().includes(searchBar.value.toLowerCase().trim()));
      searchApiProducts = searchDropdown.querySelector(".search-api-products"); 
      console.log(filteredProducts)
